c823e4deed6ee5242c946e1e8281985e5857176a
EndlessDreamerrr <tessavanderwolf@gmail.com>
2019-06-24 09:41:08 +0200

    Merge branch 'devCarl' of https://github.com/TeslaCK/MagicMazeOnline into devGedoeTessa
    # Conflicts:
    #	.idea/workspace.xml

---
+++ src/main/java/resources/supportingClasses/Buttons.java
@@ -0,0 +1,17 @@
+package resources.supportingClasses;
+
+import java.util.ArrayList;
+
+public class Button {
+
+
+    public void setButtonLocation() {
+        ArrayList<Integer>[][] buttons = new ArrayList[10][10];
+        
+    }
+
+
+}
+
+
+
