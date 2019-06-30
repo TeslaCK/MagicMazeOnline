b5c5e698e0023d4913186db522694bd0da87b1e8
carlsilaszee <C.D.Zee@tudelft.nl>
2019-06-30 18:00:37 +0200

    Finalized the MoveSet and Move class, you can make now

--- src/main/java/resources/supportingClasses/Move.java
+++ src/main/java/resources/supportingClasses/Move.java
@@ -1,20 +1,104 @@
 package resources.supportingClasses;
 
 /**
+ * This class has one variable and that is direction.
+ * The directions are: UP, RIGHT, DOWN, LEFT, ESCALATOR, PORTAL and EXPLORE.
+ *
  * @author Carl Zee
  */
 public class Move {
 
-    private static final int UP = 0, RIGHT = 1, DOWN = 2, LEFT = 3, ELEVATOR = 4, PORTAL = 5;
+    private String direction;
 
     /**
      * Default constructor
      *
      * @author Carl Zee
      */
-    public Move() {
-        //TODO implement here
+    private Move(String direction) {
+        this.direction = direction;
+
+    }
+
+    /**
+     * This makes a move with the direction up.
+     *
+     * @return Returns a Move with the direction up.
+     * @author Carl Zee
+     */
+    public static Move moveUp() {
+        Move move = new Move("UP");
+        return move;
     }
 
 
+    /**
+     * This makes a move with the direction right.
+     *
+     * @return Returns a Move with the direction right
+     * @author Carl Zee
+     */
+    public static Move moveRight() {
+        Move move = new Move("RIGHT");
+        return move;
+    }
+
+    /**
+     * This makes a move with the direction down.
+     *
+     * @return Returns a Move with the direction down.
+     * @author Carl Zee
+     */
+    public static Move moveDown() {
+        Move move = new Move("DOWN");
+        return move;
+    }
+
+    /**
+     * This makes a move with the direction left.
+     *
+     * @return Returns a Move with the direction left.
+     * @author Carl Zee
+     */
+    public static Move moveLeft() {
+        Move move = new Move("Left");
+        return move;
+    }
+
+    /**
+     * This makes a move with the direction escalator.
+     *
+     * @return Returns a Move with the direction escalator
+     * @author Carl Zee
+     */
+    public static Move moveEscalator() {
+        Move move = new Move("ESCALATOR");
+        return move;
+    }
+
+    /**
+     * This makes a move with the direction portal.
+     *
+     * @return Returns a Move with the direction portal.
+     * @author Carl Zee
+     */
+    public static Move movePortal() {
+        Move move = new Move("Portal");
+        return move;
+    }
+
+    /**
+     * This makes a move with the direction explore.
+     *
+     * @return Returns a Move with the direction explore.
+     * @author Carl Zee
+     */
+    public static Move moveExplore() {
+        Move move = new Move("EXPLORE");
+        return move;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
 }
--- src/main/java/resources/supportingClasses/MoveSet.java
+++ src/main/java/resources/supportingClasses/MoveSet.java
@@ -8,11 +8,12 @@ public class MoveSet {
     private Move[] moves;
     private int numberOfMOves;
 
-    /** Basic constructor
+    /**
+     * Basic constructor
      *
      * @author Carl Zee
      */
-    public MoveSet (){
+    public MoveSet() {
         numberOfMOves = 0;
     }
 
@@ -37,7 +38,7 @@ public class MoveSet {
      * @return Return the moves in this moveSet
      * @author Carl Zee
      */
-    public Move[] getMoves(){
+    public Move[] getMoves() {
         return moves;
     }
 
@@ -50,4 +51,92 @@ public class MoveSet {
     public int getNumberOfMoves() {
         return numberOfMOves;
     }
+
+    /**
+     * @param totalPlayers The number of moveSets.
+     * @author Carl Zee
+     */
+    public MoveSet[] initializeStartingMoveSets(int totalPlayers) {
+        if (totalPlayers > 1 && totalPlayers < 9) {
+            MoveSet[] moveSets = new MoveSet[totalPlayers];
+            switch (totalPlayers) {
+                case 2:
+                    moveSets[0].addMove(Move.moveUp());
+                    moveSets[0].addMove(Move.moveRight());
+                    moveSets[0].addMove(Move.movePortal());
+                    moveSets[1].addMove(Move.moveDown());
+                    moveSets[1].addMove(Move.moveLeft());
+                    moveSets[1].addMove(Move.moveEscalator());
+                    moveSets[1].addMove(Move.moveExplore());
+                    return moveSets;
+                case 3:
+                    moveSets[0].addMove(Move.moveLeft());
+                    moveSets[0].addMove(Move.movePortal());
+                    moveSets[1].addMove(Move.moveUp());
+                    moveSets[1].addMove(Move.moveRight());
+                    moveSets[2].addMove(Move.moveDown());
+                    moveSets[2].addMove(Move.moveEscalator());
+                    moveSets[2].addMove(Move.moveExplore());
+                    return moveSets;
+                case 4:
+                    moveSets[0].addMove(Move.moveLeft());
+                    moveSets[0].addMove(Move.movePortal());
+                    moveSets[1].addMove(Move.moveUp());
+                    moveSets[2].addMove(Move.moveRight());
+                    moveSets[2].addMove(Move.moveEscalator());
+                    moveSets[3].addMove(Move.moveDown());
+                    moveSets[3].addMove(Move.moveExplore());
+                    return moveSets;
+                case 5:
+                    moveSets[0].addMove(Move.moveLeft());
+                    moveSets[0].addMove(Move.movePortal());
+                    moveSets[1].addMove(Move.moveUp());
+                    moveSets[2].addMove(Move.moveRight());
+                    moveSets[2].addMove(Move.moveEscalator());
+                    moveSets[3].addMove(Move.moveDown());
+                    moveSets[3].addMove(Move.moveExplore());
+                    moveSets[4].addMove(Move.moveLeft());
+                    return moveSets;
+                case 6:
+                    moveSets[0].addMove(Move.moveLeft());
+                    moveSets[0].addMove(Move.movePortal());
+                    moveSets[1].addMove(Move.moveUp());
+                    moveSets[2].addMove(Move.moveRight());
+                    moveSets[2].addMove(Move.moveEscalator());
+                    moveSets[3].addMove(Move.moveDown());
+                    moveSets[3].addMove(Move.moveExplore());
+                    moveSets[4].addMove(Move.moveLeft());
+                    moveSets[5].addMove(Move.moveRight());
+                    return moveSets;
+                case 7:
+                    moveSets[0].addMove(Move.moveLeft());
+                    moveSets[0].addMove(Move.movePortal());
+                    moveSets[1].addMove(Move.moveUp());
+                    moveSets[2].addMove(Move.moveRight());
+                    moveSets[2].addMove(Move.moveEscalator());
+                    moveSets[3].addMove(Move.moveDown());
+                    moveSets[3].addMove(Move.moveExplore());
+                    moveSets[4].addMove(Move.moveLeft());
+                    moveSets[5].addMove(Move.moveRight());
+                    moveSets[6].addMove(Move.moveDown());
+                    return moveSets;
+                case 8:
+                    moveSets[0].addMove(Move.moveLeft());
+                    moveSets[0].addMove(Move.movePortal());
+                    moveSets[1].addMove(Move.moveUp());
+                    moveSets[2].addMove(Move.moveRight());
+                    moveSets[2].addMove(Move.moveEscalator());
+                    moveSets[3].addMove(Move.moveDown());
+                    moveSets[3].addMove(Move.moveExplore());
+                    moveSets[4].addMove(Move.moveLeft());
+                    moveSets[5].addMove(Move.moveRight());
+                    moveSets[6].addMove(Move.moveDown());
+                    moveSets[7].addMove(Move.moveUp());
+                    return moveSets;
+            }
+        }
+        System.out.println("Total players isn't between 1 and 9. It is: " + totalPlayers);
+        return null;
+    }
+
 }
