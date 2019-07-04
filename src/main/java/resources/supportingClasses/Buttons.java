7aabefc29186dcfffba42efc9d8e4e25f597ea73
EndlessDreamerrr <tessavanderwolf@gmail.com>
2019-07-04 15:37:54 +0200

    Changes in the buttons class

--- src/main/java/resources/supportingClasses/Buttons.java
+++ src/main/java/resources/supportingClasses/Buttons.java
@@ -1,18 +1,14 @@
 package resources.supportingClasses;
 
-
-import com.sun.xml.internal.bind.v2.TODO;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
+import javafx.scene.effect.DropShadow;
 import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import models.BoardModel;
 import views.GameView;
-
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
-
 
 /**
  * @author Tessa
@@ -20,7 +16,6 @@ import java.util.Scanner;
 
 public class Buttons {
     //2D array List
-    List<List<Integer>> testArr = new ArrayList<List<Integer>>();
 
     private final int CHARACTERIDGREEN = 10;
 
@@ -37,9 +32,10 @@ public class Buttons {
     private final String YELLOWCHARACTERSTYLE = "-fx-background-color: #FFFF00; ";
 
     //Tile styles
-    private final String TILESTYLEGROUND = "-fx-background-color: #FFE5CC; ";
+    private final String TILESTYLEGROUND = "-fx-background-color: #FFE5CC;";
     private final String TILESTYLEDOOR = "-fx-background-color: #FFB266; ";
-    private final String TILESTYLEWALL = "-fx-background-color: #994C00; ";
+    //private final String TILESTYLEWALL = "-fx-background-color: #994C00; ";
+    private final String TILESTYLEWALL = "-fx-background-color: #A9A9A9; ";
     private final String TILESTYLEITEMGREEN = "-fx-background-color: #99FF99; ";
     private final String TILESTYLEEXITGREEN = "-fx-background-color: #00FF00; ";
     private final String TILESTYLEITEMPURPLE = "-fx-background-color: #00FF00; ";
@@ -51,15 +47,16 @@ public class Buttons {
 
     /*
     TODO @Tessa
-    Buttons mogen iets groter       -> Button resize
+    Buttons mogen iets groter       -> Button resize                        GED
     Afbeeldingen                    -> Afbeeldingen toevoegen aan buttons
     Kan worden ingedrukt lijntje    -> Clickable buttons zichbaarder maken door bijv en randje
 
      */
-
     //Path of the gameBoard and characterLocations
     private final String BOARDPATH = "src\\main\\java\\resources\\gameBoards\\startingBoard1.txt";
     private final String CHARACTERSPATH = "src\\main\\java\\resources\\gameBoards\\startingCharacters1.txt";
+
+//    private final String groundTilePath = "src\\main\\java\\resources\\images\\groundTile.jpg";
 
     //This needs a boardModel to load the board and characters
     private BoardModel boardModel;
@@ -80,13 +77,23 @@ public class Buttons {
     /**
      * This will create a VBOX with a button for each tile of the board.
      * The buttons will call GameView.buttonClicked(x, y, button) when there are clicked.
-     *
      * @return Returns a VBox with a button for each boardTile
      * @author Carl Zee & Tessa van de Wolf
      */
     public VBox buttons() {
         //initialises variables
         VBox vBox = new VBox();
+
+        int depth = 25;
+
+        DropShadow borderGlow = new DropShadow();
+        borderGlow.setOffsetY(-2f);
+        borderGlow.setOffsetX(-2f);
+        borderGlow.setColor(Color.BLUE);
+        borderGlow.setWidth(depth);
+        borderGlow.setHeight(depth);
+
+
         int[][] boardArray = boardModel.getBoard();
         HBox[] hBoxes = new HBox[boardArray.length];
         for (int i = 0; i < boardArray.length; i++) {
@@ -97,11 +104,12 @@ public class Buttons {
             for (int x = 0; x < boardArray[y].length; x++) {
                 Button tempButton;
 
-
                 switch (boardArray[y][x]) {
                     case 0:
                         tempButton = new Button("0");
+                        tempButton.setEffect(borderGlow);
                         tempButton.setStyle(TILESTYLEGROUND);
+
                         int isCharacter = isCharacter(x, y);
                         if (isCharacter != -1) {
                             switch (isCharacter) {
@@ -123,7 +131,6 @@ public class Buttons {
                     case 1:
                         tempButton = new Button("1");
                         tempButton.setStyle(TILESTYLEDOOR);
-
                         break;
                     case 2:
                         tempButton = new Button("2");
@@ -166,12 +173,18 @@ public class Buttons {
                 tempButton.setOnAction(new EventHandler<ActionEvent>() {
                     @Override
                     public void handle(ActionEvent event) {
+
                         String idButton = finalTempButton.getId();
                         Scanner scanner = new Scanner(idButton);
                         GameView.buttonClicked(scanner.nextInt(), scanner.nextInt(), finalTempButton);
+                        System.out.println("yes");
+                        
+
                     }
                 });
                 hBoxes[y].getChildren().add(tempButton);
+                tempButton.setMinHeight(80);
+                tempButton.setMinWidth(80);
             }
         }
         for (int i = 0; i < hBoxes.length; i++) {
@@ -195,6 +208,7 @@ public class Buttons {
             if (characterLocations[i] != null) {
                 if (x == characterLocations[i].getLocation().getX() && y == characterLocations[i].getLocation().getY()) {
                     return characterLocations[i].getCharacterID();
+
                 }
             }
         }
